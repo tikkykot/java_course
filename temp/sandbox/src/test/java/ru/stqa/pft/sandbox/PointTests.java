ec9551af22901d380ac96487ee6665afeab4eebc
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-08-23 12:32:49 +0300

    Задание #3

--- temp/sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ temp/sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,8 +5,20 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testDistance(){
+  public void testDistance() {
     PointData P = new PointData(0, 1, 2, -2);
-    assert () == 2.23606797749979;
+    assert Point.distance(P) == 2.23606797749979;
+  }
+
+   @Test
+    public void testDistance1 () {
+      PointData P = new PointData(0, 1, 3, -3);
+      assert Point.distance(P) == 3.1622776601683795;
+    }
+
+  @Test
+  public void testDistance2 () {
+    PointData P = new PointData(0, 1, 0, -1);
+    assert Point.distance(P) == 1.4142135623730951;
   }
 }
