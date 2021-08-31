66149ea246c22adfd6f35db73f63240477d7445c
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-08-31 19:56:40 +0300

    Создан класс TestBase, в него перенесены методы для создания и удаления групп

---
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package ru.stqu.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
+
