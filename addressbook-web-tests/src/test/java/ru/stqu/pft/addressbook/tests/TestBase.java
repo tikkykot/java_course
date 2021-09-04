93ab8ab9a7cb7f61dd04d47caa2ed004d17c286b
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-09-04 15:02:59 +0300

    Задание №7

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqu.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqu.pft.addressbook.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
