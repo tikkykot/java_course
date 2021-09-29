c0fe06bdb73dd9ff5c987bd11fc1956779a2e653
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-09-29 19:12:24 +0300

    6.5_success

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/UserCreationTests.java
@@ -6,7 +6,7 @@ import ru.stqu.pft.addressbook.model.GroupData;
 import ru.stqu.pft.addressbook.model.UserData;
 import ru.stqu.pft.addressbook.model.Users;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,12 +17,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class UserCreationTests extends TestBase{
 
   @DataProvider
-  public Iterator<Object[]> validUsers() {
+  public Iterator<Object[]> validUsers() throws IOException {
     File photo = new File("src/test/resources/stru.png");
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new UserData().withFirstname("Dmitry1").withLastname("Zagumenny1").withAddress("Saint_Petersburg1").withPhone_home("+7123456789-1").withEmail("qa@java1.com").withGroup("test1").withPhoto(photo)});
-    list.add(new Object[] {new UserData().withFirstname("Dmitry2").withLastname("Zagumenny2").withAddress("Saint_Petersburg2").withPhone_home("+7123456789-2").withEmail("qa@java2.com").withGroup("test2").withPhoto(photo)});
-    list.add(new Object[] {new UserData().withFirstname("Dmitry3").withLastname("Zagumenny3").withAddress("Saint_Petersburg3").withPhone_home("+7123456780-3").withEmail("qa@java3.com").withGroup("test3").withPhoto(photo)});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {(new UserData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+              .withPhone_home(split[3]).withEmail(split[4]).withGroup(split[5]).withPhoto(photo))});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
