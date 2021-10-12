68be7935ed6f94f0f6e35994399a2c23d7a4a926
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-10-12 18:32:12 +0300

    8.3_ok

--- mantis-tests/src/test/java/ru/stqa/mantis/pft/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/pft/appmanager/ApplicationManager.java
@@ -49,6 +49,14 @@ public class ApplicationManager {
     wd.quit();
   }
 
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
   private boolean isElementPresent(By by) {
     try {
       wd.findElement(by);
