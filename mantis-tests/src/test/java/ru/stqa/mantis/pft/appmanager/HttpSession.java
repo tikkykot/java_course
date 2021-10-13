e0808dae2d409742472638250a64c8e261bbe481
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-10-13 11:17:25 +0300

    8.4 работает

--- mantis-tests/src/test/java/ru/stqa/mantis/pft/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/pft/appmanager/HttpSession.java
@@ -25,7 +25,7 @@ public class HttpSession {
     }
 
     public boolean login(String username, String password) throws IOException {
-      HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login_page.php");
+      HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
       List<NameValuePair> params = new ArrayList<>();
       params.add(new BasicNameValuePair("username", username));
       params.add(new BasicNameValuePair("password", password));
@@ -34,7 +34,7 @@ public class HttpSession {
       post.setEntity(new UrlEncodedFormEntity(params));
       CloseableHttpResponse response = httpClient.execute(post);
       String body = geTextFrom(response);
-      return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+      return body.contains(String.format("<a href=\"/mantisbt-2.25.2/mantisbt-2.25.2/account_page.php\">%s</a>", username));
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -46,10 +46,11 @@ public class HttpSession {
     }
 
   public boolean isLoggedInAs(String username) throws IOException {
-    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/login_page.php");
+    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/login.php");
     CloseableHttpResponse response = httpClient.execute(get);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+    return body.contains(String.format("<a href=\"/mantisbt-2.25.2/mantisbt-2.25.2/account_page.php\">%s</a>", username));
+
     }
   }
 
