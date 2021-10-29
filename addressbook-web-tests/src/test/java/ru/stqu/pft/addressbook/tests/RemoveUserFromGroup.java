5bc87ecca97d3ac781162750fd528b1c65b7addd
Dmitry Zagumenny <dmitry.zagumenny@kofax.com>
2021-10-29 18:03:14 +0300

    Задание №16_очередная попытка

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/RemoveUserFromGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/RemoveUserFromGroup.java
@@ -25,10 +25,29 @@ public class RemoveUserFromGroup extends TestBase {
   //  }
   //  helper.addUserToGroupTest();
   //  }
-public void ensurePreconditions() {
-    UserData contact = app.db().users().iterator().next();
-    GroupData groupToAdd = new AddUserToGroup().prepareGroupsBeforeOperation(contact);
-    app.contact().addToGroup(contact, groupToAdd);}
+  //public void ensurePreconditions() {
+  // UserData contact = app.db().users().iterator().next();
+  //  GroupData groupToAdd = new AddUserToGroup().prepareGroupsBeforeOperation(contact);
+  //  app.contact().addToGroup(contact, groupToAdd);}
+
+  public void ensurePreconditions() {
+    if (app.db().users().size() == 0) {
+      if (app.db().groups().size() == 0) {
+        app.goTo().groupPage();
+        app.group().create(new GroupData().withName("test1"));
+      }
+      Groups groups = app.db().groups();
+      app.returnToHomePage();
+      app.contact().initUserCreation();
+      app.contact().create(new UserData()
+              .withFirstname("Dmitry").withLastname("Zagumenny").withAddress("Saint_Petersburg").withPhone_home("+7123456789").withEmail("qa@java.com")
+              .inGroup(groups.iterator().next()));
+      app.returnToHomePage();
+      UserData contact = app.db().users().iterator().next();
+      app.contact().addToGroup(contact, groups.iterator().next());
+      app.returnToHomePage();
+    }
+  }
 
   @Test
   public void removeUserFromGroupTest(){
